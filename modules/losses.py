8bbe08dfd909cf56600ac11e0dc2814d877248fc
Donatien Delehlle <ddonatien@hkclr.hk>
2021-06-11 14:48:29 +0800

    Added logcosh loss for ed

---
+++ modules/losses.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+class LogCoshLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_t, y_prime_t):
+        ey_t = y_t - y_prime_t
+        return torch.mean(torch.log(torch.cosh(ey_t + 1e-12)))
