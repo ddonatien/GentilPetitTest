dcd079f1adff6877ae9b3b39fa5c995ca5cc7a79
Donatien Delehlle <ddonatien@hkclr.hk>
2021-06-23 13:03:17 +0800

    Refactoring

--- modules/convED.py
+++ modules/convED.py
@@ -4,15 +4,9 @@ class ConvEncoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),  # b, 64, 64, 64
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),  # b, 64, 32, 32
-            nn.Conv2d(64, 32, 3, padding=1),  # b, 32, 32, 32
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),  # b, 32, 16, 16
-            nn.Conv2d(32, 16, 3, padding=1),  # b, 16, 16, 16
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),  # b, 16, 8, 8
+            ConvReluPool(3, 64), # C1 => bx64x32x32
+            ConvReluPool(64, 32), # C2 => bx32x16x16
+            ConvReluPool(32, 16), # C3 => bx16x8x8
             nn.Flatten()
         )
 
@@ -25,6 +19,9 @@ class ConvDecoder(nn.Module):
         super().__init__()
         self.decoder = nn.Sequential(
             nn.Unflatten(1, (16, 8, 8)), #b, 16, 8, 8
+            # DeconvRelu(16, 32), # D1 => 32x*x*
+            # DeconvRelu(32, 64), # D2 => 64x*x*
+            # DeconvRelu(64, 3), # D3 => 3x*x*
             nn.ConvTranspose2d(16, 32, 3, stride=2),  # b, 32, 8, 8
             nn.ReLU(True),
             nn.ConvTranspose2d(32, 64, 3, stride=2, padding=1),  # b, 64, x, x
@@ -50,6 +47,26 @@ class PrintLayer(nn.Module):
 class CropLayer(nn.Module):
     def forward(self, inputs):
         return inputs[:, :, :64, :64]
+
+class ConvReluPool(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_features, out_features, 3, padding=1),  # b, 64, 64, 64
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 64, 32, 32
+        )
+    
+    def forward(self, inputs):
+        return self.layer(inputs)
+
+class DeconvRelu(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.ConvTranspose2d(in_features, out_features, 3, stride=2),  # b, 32, 8, 8
+            nn.ReLU(True),
+        )
 
 class ConvED(nn.Module):
     def __init__(self):
